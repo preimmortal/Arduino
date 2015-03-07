c42f332e0c152f6c32d259bd98f9ae2ffcd95dea
preimmortal <preimmortal@gmail.com>
2015-03-07 03:48:50 -0800

    fixed bluetooth issue

--- CSE237A_Proj/CSE237A_Proj.ino
+++ CSE237A_Proj/CSE237A_Proj.ino
@@ -1,41 +1,85 @@
-
+//Library Includes
 #include <dht11.h>
 #include <temp.h>
 #include <Timer.h>
 #include <SoftwareSerial.h>
+#include <avr/pgmspace.h>
+
+//#define DEBUG
+
+/***************************************/
+/* Bluetooth Init */
+/***************************************/
 
 //Bluetooth Initalization Parameters
-const int rxPin = 2; //SoftwaremySerial RX pin, connect to JY-MCY TX pin
-const int txPin = 7; //SoftwaremySerial TX pin, connect to JY-MCU RX pin
+const int rxPin = 2; //SoftwaremySerial RX pin, connect to JY-MCY TX pin (Green)
+const int txPin = 7; //SoftwaremySerial TX pin, connect to JY-MCU RX pin (Red)
                      // level shifting to 3.3 volts may be needed
 
 SoftwareSerial mySerial(rxPin, txPin); // RX, TX
-int state = 0;        // if state is 1, the LED will turn on and
-                      // if state is 0, the LED will turn off
-int flag = 0;         // a flag to prevent duplicate messages
 
+
+/***************************************/
+/* DHT11 Init */
+/***************************************/
 dht11 DHT11;
-
 #define DHT11PIN 8
 
-int soundSensorPin=A5;
+/***************************************/
+/* Sound Init */
+/***************************************/
+int soundSensorPin=A0;
 int soundReading=0;
-int soundThreshold=20;
-
+int SOUND_THRESH = 6;
+/***************************************/
+/* Motion Init */
+/***************************************/
 int HCSR501PIN=4;
 
+/***************************************/
+/* LED Init */
+/***************************************/
 int LEDPIN = 13;
+int LEDSOUNDPIN = 12;
 
+
+/***************************************/
+/* Timer Init */
+/***************************************/
 Timer t;
 
-//Sensor Buffers
-float temp_buffer[10]; //5 Minutes of samples
+
+/***************************************/
+/* Buffers Init */
+/***************************************/
+//Buffer Sizes
+#define TEMP_BUF_SIZE 10
+#define MOTION_BUF_SIZE 60
+#define SOUND_BUF_SIZE 750
+//Initialize Buffers
+float temp_buffer[TEMP_BUF_SIZE]; //5 Minutes of samples - Stored in deg C
+float humidity_buffer[TEMP_BUF_SIZE]; // 5 Minutes of samples
 //These Buffes must store 1 bit at a time
-byte motion_buffer[60]; //1 Minute of samples (motion)
-byte sound_buffer[750]; //1 Minute of samples (sound)
+byte motion_buffer[MOTION_BUF_SIZE]; //1 Minute of samples (motion)
+byte sound_buffer[SOUND_BUF_SIZE]; //1 Minute of samples (sound)
+
+/***************************************/
+/* Buffer Indices Init */
+/***************************************/
+static int DHT11_BUF_IDX = 0;
+static int MOTION_BUF_IDX = 0;
+static int SOUND_BUF_IDX = 0;
+
+/***************************************/
+/* Output Flags Init */
+/***************************************/
+byte SOUND_FLAG = 0;
+byte MOTION_FLAG = 0;
 
 
-
+/***************************************/
+/* Setup Function */
+/***************************************/
 void setup()
 {
   //Turn on Bluetooth mySerial
@@ -48,13 +92,13 @@ void setup()
   mySerial.println("EventID: ");
   mySerial.println(tempReadEvent);
   
-  /*
+  
   //Motion Sensor: 100ms sample rate
   int motionReadEvent = t.every(100, HC_SR501_Reading);
   mySerial.println("Started Motion Reading");
   mySerial.println("EventID: ");
   mySerial.println(motionReadEvent);
-  */
+  
   
   //Sound Sensor: 10ms sample rate
   int soundReadEvent = t.every(10, Sound_Sensor_Reading);
@@ -68,9 +112,7 @@ void setup()
   mySerial.println("EventID: ");
   mySerial.println(clearScreenEvent);
   */
-  
-  
-  
+
   //HC_SR501 - Motion Sensor Init
   pinMode(HCSR501PIN,INPUT);
   
@@ -79,8 +121,9 @@ void setup()
   
   //LED Pin
   pinMode(LEDPIN, OUTPUT);
+  pinMode(LEDSOUNDPIN, OUTPUT);
   //Turn LED Off
-  digitalWrite(LEDPIN, LOW);
+  //digitalWrite(LEDPIN, LOW);
 }
 
 void Clear_Screen()
@@ -94,11 +137,13 @@ void Clear_Screen()
 
 void DHT11_Reading()
 {
+  //static int DHT_READ_IDX = 0;
   //DHT_11 Main Code
   mySerial.println("\n");
 
   int chk = DHT11.read(DHT11PIN);
 
+/*
   mySerial.print("Read sensor: ");
   switch (chk)
   {
@@ -133,9 +178,25 @@ void DHT11_Reading()
 
   mySerial.print("Dew PointFast (C): ");
   mySerial.println(dewPointFast(DHT11.temperature, DHT11.humidity));
+  */
+  
+  temp_buffer[DHT11_BUF_IDX] = (float)DHT11.temperature;
+  humidity_buffer[DHT11_BUF_IDX] = (float) DHT11.humidity;
+  
+  /****DEBUG CODE****/
+  #ifdef DEBUG
+  Serial.print("Wrote temp_buffer:\n");
+  Serial.println(DHT11_BUF_IDX);
+  Serial.println(temp_buffer[DHT11_BUF_IDX]);
+  #endif
+  /****DEBUG CODE****/
+  
+  DHT11_BUF_IDX++;
+  if(DHT11_BUF_IDX>=TEMP_BUF_SIZE){
+    DHT11_BUF_IDX = 0; 
+  }
   
   
-
 }
 
 void HC_SR501_Reading()
@@ -144,29 +205,153 @@ void HC_SR501_Reading()
   //HC_SR501 Main Code
   //digitalWrite(13,digitalRead(2));
   if(digitalRead(HCSR501PIN)){
-    mySerial.println("Motion: 1\n");
+    int BUF_IDX = MOTION_BUF_IDX / 8;
+    int SUB_IDX = MOTION_BUF_IDX % 8;
+    byte WriteVal = 1<<SUB_IDX;
+    motion_buffer[BUF_IDX] |= WriteVal;
+    
+    /****DEBUG CODE****/
+    #ifdef DEBUG
+    //mySerial.println("Motion: 1");
+    Serial.println("Motion: 1");
+    Serial.print("Wrote motion_buffer:");
+    Serial.println(BUF_IDX);
+    Serial.println(SUB_IDX);
+    Serial.println(motion_buffer[BUF_IDX]);
+    Serial.println("\n");
+    #endif
+    /****DEBUG CODE****/
+    if(MOTION_FLAG == 1){
+      Serial.println("Motion: 1");
+      mySerial.println("Motion: 1"); 
+    }
   }else{
-    mySerial.println("Motion: 0\n"); 
+    int BUF_IDX = MOTION_BUF_IDX / 8;
+    int SUB_IDX = MOTION_BUF_IDX % 8;
+    byte WriteVal = 1<<SUB_IDX;
+    WriteVal = ~WriteVal;
+    motion_buffer[BUF_IDX] &= WriteVal;
+    
+    /****DEBUG CODE****/
+    #ifdef DEBUG
+    //mySerial.println("Motion: 0");
+    Serial.println("Motion: 0");
+    Serial.print("Wrote motion_buffer:");
+    Serial.println(BUF_IDX);
+    Serial.println(SUB_IDX);
+    Serial.println(motion_buffer[BUF_IDX]);
+    Serial.println("\n");
+    #endif
+    /****DEBUG CODE****/
+    if(MOTION_FLAG == 1){
+      Serial.println("Motion: 0");
+      mySerial.println("Motion: 0"); 
+    }
+  }
+  //Increment Index
+  MOTION_BUF_IDX++;
+  if(MOTION_BUF_IDX >= 8*MOTION_BUF_SIZE){
+    MOTION_BUF_IDX=0; 
   }
 }
 
 void Sound_Sensor_Reading()
 {
   //Sound Sensor Main Code
+  
   soundReading = analogRead(soundSensorPin);
-  if(soundReading >1){
+  
+  if(soundReading >1 && SOUND_FLAG==1){
     mySerial.print("Sound Reading: ");
     mySerial.println(soundReading);
     Serial.print("Sound Reading: ");
     Serial.println(soundReading);
   }
   
-  if(soundReading > 10){
-    digitalWrite(LEDPIN, HIGH);
+  
+  //Sample into Buffers
+  if(soundReading > SOUND_THRESH){
+    int BUF_IDX = SOUND_BUF_IDX / 8;
+    int SUB_IDX = SOUND_BUF_IDX % 8;
+    byte WriteVal = 1<<SUB_IDX;
+    sound_buffer[BUF_IDX] |= WriteVal;
+    
+    //****DEBUG CODE****
+    #ifdef DEBUG
+    //mySerial.println("Sound: 1");
+    //Serial.println("Sound: 1");
+    Serial.print("Wrote sound_buffer:");
+    Serial.println(BUF_IDX);
+    Serial.println(SUB_IDX);
+    Serial.println(sound_buffer[BUF_IDX]);
+    //Serial.println("\n");
+    #endif
+    //****DEBUG CODE****
   }else{
-    digitalWrite(LEDPIN, LOW); 
+    int BUF_IDX = SOUND_BUF_IDX / 8;
+    int SUB_IDX = SOUND_BUF_IDX % 8;
+    byte WriteVal = 1<<SUB_IDX;
+    WriteVal = ~WriteVal;
+    sound_buffer[BUF_IDX] &= WriteVal;
+    
+    //****DEBUG CODE****
+    #ifdef DEBUG
+    //mySerial.println("Sound: 0");
+    //Serial.println("Sound: 0");
+    Serial.print("Wrote sound_buffer:");
+    Serial.println(BUF_IDX);
+    Serial.println(SUB_IDX);
+    Serial.println(sound_buffer[BUF_IDX]);
+    //Serial.println("\n");
+    #endif
+    //****DEBUG CODE****
+  }  
+  SOUND_BUF_IDX++;
+  if(SOUND_BUF_IDX >= 8*SOUND_BUF_SIZE){
+    SOUND_BUF_IDX=0; 
   }
   
+  //Try to make LED stay on longer
+  //LED CIRCUIT
+  static int sampleNum = 0;
+  sampleNum++;
+  if(soundReading > SOUND_THRESH){
+    //Serial.println("HIGH Sound Reading:");
+    //Serial.println(soundReading);
+    digitalWrite(LEDSOUNDPIN, HIGH);
+    sampleNum = 0;
+  }else if(sampleNum >= 5){
+    //Serial.println("LOW Sound Reading:");
+    //Serial.println(soundReading);
+    digitalWrite(LEDSOUNDPIN, LOW); 
+  }
+  
+}
+
+void DHT11_Value(){
+  int i;
+  Serial.println("Last 10 T/H Readings");
+  mySerial.println("Last 10 T/H Readings");
+  
+  for(i=0; i<TEMP_BUF_SIZE; i++){
+    Serial.print(i);
+    Serial.print(" -- "); 
+    Serial.print("Temp: ");
+    Serial.print(temp_buffer[i]); 
+    Serial.print(" C -- ");
+    Serial.print("Humidity: ");
+    Serial.print(humidity_buffer[i]);
+    Serial.println(" percent");
+    
+    mySerial.print(i);
+    mySerial.print(" -- "); 
+    mySerial.print("Temp: ");
+    mySerial.print(temp_buffer[i]); 
+    mySerial.print(" C -- ");
+    mySerial.print("Humidity: ");
+    mySerial.print(humidity_buffer[i]);
+    mySerial.println("percent");
+  }
 }
 
 void Bluetooth_Module()
@@ -174,32 +359,37 @@ void Bluetooth_Module()
   typedef enum {SM_IDLE, READ_TEMP, READ_MOTION, READ_SOUND} STATES;
   //Implement State Machine
   static STATES STATE = SM_IDLE;
-  
+  STATES NEXT_STATE = STATE;
+  int event = 0;
+
   //State Transition Logic
   if(mySerial.available() > 0){
-    state = mySerial.read();
-    switch(state){
+    event = mySerial.read();
+    switch(event){
       case '0': 
         mySerial.println("Switching to IDLE State\n");
-        STATE = SM_IDLE;
+        Serial.println("Switching to IDLE State\n");
+        NEXT_STATE = SM_IDLE;
         break;
       case '1':
         mySerial.println("Switching to TEMP State\n");
-        STATE = READ_TEMP;
+        Serial.println("Switching to TEMP State\n");
+        NEXT_STATE = READ_TEMP;
         break;
       case '2':
         mySerial.println("Switching to MOTION State\n");
-        STATE = READ_MOTION;
+        Serial.println("Switching to MOTION State\n");
+        NEXT_STATE = READ_MOTION;
         break;
       case '3':
         mySerial.println("Switching to SOUND State\n");
-        STATE = READ_SOUND;
+        Serial.println("Switching to SOUND State\n");
+        NEXT_STATE = READ_SOUND;
         break;
       default:
-        STATE = SM_IDLE;
+        NEXT_STATE = SM_IDLE;
         break;
     }
-    flag=0; //clear the flag so we can print the state
   }
   
   switch(STATE){
@@ -208,6 +398,12 @@ void Bluetooth_Module()
       mySerial.println("1 -> Temp Readings\n");
       mySerial.println("2 -> Motion Sensor Readings\n");
       mySerial.println("3 -> Sound Sensor Readings\n");
+      
+      Serial.println("IDLE State\n");
+      Serial.println("1 -> Temp Readings\n");
+      Serial.println("2 -> Motion Sensor Readings\n");
+      Serial.println("3 -> Sound Sensor Readings\n");
+      
       //Delay
       delay(1000);
       //Clear Screen
@@ -215,11 +411,21 @@ void Bluetooth_Module()
       mySerial.print("[2J");    // clear screen command
       mySerial.write(27);
       mySerial.print("[H");     // cursor to home command
+      
+      //Clear Screen
+      Serial.write(27);       // ESC command
+      Serial.print("[2J");    // clear screen command
+      Serial.write(27);
+      Serial.print("[H");     // cursor to home command
       break;
     case READ_TEMP:
-      //DHT11_Reading();
-      //Delay
+      DHT11_Value();
       delay(1000);
+      //Clear Screen
+      Serial.write(27);       // ESC command
+      Serial.print("[2J");    // clear screen command
+      Serial.write(27);
+      Serial.print("[H");     // cursor to home command
       //Clear Screen
       mySerial.write(27);       // ESC command
       mySerial.print("[2J");    // clear screen command
@@ -229,70 +435,32 @@ void Bluetooth_Module()
     case READ_MOTION:
       //HC_SR501_Reading();
       //Delay
-      delay(100);
-      //Clear Screen
-      mySerial.write(27);       // ESC command
-      mySerial.print("[2J");    // clear screen command
-      mySerial.write(27);
-      mySerial.print("[H");     // cursor to home command
+      MOTION_FLAG = 1;
+      if(NEXT_STATE != READ_MOTION){
+         MOTION_FLAG = 0; 
+      }
       break;
     case READ_SOUND:
       //Sound_Sensor_Reading();
-      //Delay
-      delay(10);
+      SOUND_FLAG = 1;
+      if(NEXT_STATE != READ_SOUND){
+         SOUND_FLAG = 0; 
+      }
       break;
   }
   
-  
+  STATE = NEXT_STATE;
   
 }
 
 void loop()
-{
-  //Temperature Readings
-  //DHT11_Reading();
-  
-  //Motion Sensor Readings
-  //HC_SR501_Reading();
-  
-  //Sound Sensor Readings
-  //Sound_Sensor_Reading();
-  
+{ 
   //Bluetooth Module
   //This module will control when we read from sensors
-  //Bluetooth_Module();
+  Bluetooth_Module();
   
-  /*
-  int i;
-  for(i=0; i < 10; i++){
-    temp_buffer[i] = 1;
-    mySerial.print("TempBuffer=");
-    mySerial.println(i);
-    mySerial.println(temp_buffer[i]);
-  }
-  for(i=0; i<600; i++){
-    motion_buffer[i] = 1; 
-    mySerial.print("motionbuffer=");
-    mySerial.println(i);
-    mySerial.println(motion_buffer[i]);
-  }
-  
-  for(i=0; i<600; i++){
-    motion_buffer[i] = 1; 
-    mySerial.print("soundbuffer=");
-    mySerial.println(i);
-    mySerial.println(sound_buffer[i]);
-  }
-  */
-  
+  //Update the Timer for interrupts
   t.update();
-  /*
-  //Clear Screen
-  mySerial.write(27);       // ESC command
-  mySerial.print("[2J");    // clear screen command
-  mySerial.write(27);
-  mySerial.print("[H");     // cursor to home command
-  */
 }
 
 //
